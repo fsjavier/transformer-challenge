235f93ba69b6f28b1f8e5407e2db353196db9644
fsjavier <javier.fersal@gmail.com>
2024-09-09 22:15:54 +0200

    Create urls and views to retrieve headers and content from files

--- backend/csv_manager/urls.py
+++ backend/csv_manager/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import CSVFileListCreateView
+from .views import (
+  CSVFileListCreateView,
+  CSVFileRetrieveHeaderView,
+  CSVFileRetrieveContentView
+)
 
 app_name = 'csv_manager'
 
 urlpatterns = [
     path('csv-files/', CSVFileListCreateView.as_view(), name='csv_file_list_create'),
+    path('csv-files/<int:pk>/headers/', CSVFileRetrieveHeaderView.as_view(), name='csv_file_headers'),
+    path('csv-files/<int:pk>/content/', CSVFileRetrieveContentView.as_view(), name='csv_file_content'),
 ]
--- backend/csv_manager/views.py
+++ backend/csv_manager/views.py
@@ -1,7 +1,70 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+from django.http import FileResponse
 from .models import CSVFile
 from .serializers import CSVFileSerializer
+import csv
+from io import StringIO
 
 class CSVFileListCreateView(generics.ListCreateAPIView):
+    """
+    List all CSV files or create a new CSV file.
+    """
     queryset = CSVFile.objects.all()
     serializer_class = CSVFileSerializer
+
+class CSVFileRetrieveHeaderView(generics.RetrieveAPIView):
+    """
+    Retrieve the header of a CSV file.
+    """
+    queryset = CSVFile.objects.all()
+    serializer_class = CSVFileSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        csv_file = self.get_object()
+        try:
+            with csv_file.file.open('rb') as f:
+                csv_content = f.read().decode('utf-8')
+            csv_reader = csv.reader(StringIO(csv_content))
+            headers = next(csv_reader)
+
+            return Response({
+                'id': csv_file.id,
+                'name': csv_file.name,
+                'headers': headers
+            })
+
+        except Exception as e:
+            return Response(
+                {'error': f"Error reading the header of the file: {str(e)}"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+class CSVFileRetrieveContentView(generics.RetrieveAPIView):
+    """
+    Retrieve the content of a CSV file.
+    """
+    queryset = CSVFile.objects.all()
+    serializer_class = CSVFileSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        csv_file = self.get_object()
+        try:
+            with csv_file.file.open('rb') as f:
+                csv_content = f.read().decode('utf-8')
+            csv_reader = csv.reader(StringIO(csv_content))
+            headers = next(csv_reader)
+            rows = list(csv_reader)
+
+            return Response({
+            'id': csv_file.id,
+            'name': csv_file.name,
+            'headers': headers,
+            'rows': rows
+            })
+        except Exception as e:
+            return Response(
+                {'error': f"Error reading the content of the file: {str(e)}"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
