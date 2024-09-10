4a7e83fb2754d80715c96021f03182ee85125353
fsjavier <javier.fersal@gmail.com>
2024-09-10 11:14:06 +0200

    Add header component

---
+++ frontend/app/_components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-primary text-text-primary flex items-center justify-center p-4 col-span-2 border-b border-border-light">
+      <h1 className="text-2xl font-bold">CSV File Manager</h1>
+    </header>
+  );
+}
