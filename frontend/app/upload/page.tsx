c95092c22f462f13d0c9e4eab0efb8be7dda4857
fsjavier <javier.fersal@gmail.com>
2024-09-10 14:04:43 +0200

    Add form component to upload page

--- frontend/app/upload/page.tsx
+++ frontend/app/upload/page.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import { Metadata } from "next";
+import UploadForm from "@/app/_components/UploadForm";
 
-export default function Page() {
-  return <div>Upload data</div>;
+export const metadata: Metadata = {
+  title: "Upload CSV File | CSV File Manager",
+  description: "Upload your CSV files for processing and analysis.",
+};
+
+export default function UploadPage() {
+  return (
+    <div className="max-w-md mx-auto p-6 bg-background-light rounded-lg shadow-md">
+      <h1 className="text-2xl font-bold mb-6 text-text-dark">
+        Upload CSV File
+      </h1>
+      <UploadForm />
+    </div>
+  );
 }
