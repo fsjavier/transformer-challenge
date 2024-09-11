e65bfc99a8fe83afc6586776f3119a04c5aecebb
fsjavier <javier.fersal@gmail.com>
2024-09-11 14:24:28 +0200

    Add urls for enrichment and check enrichment status

--- backend/csv_manager/urls.py
+++ backend/csv_manager/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import (
   CSVFileListCreateView,
   CSVFileRetrieveHeaderView,
-  CSVFileRetrieveContentView
+  CSVFileRetrieveContentView,
+  CSVFileEnrichView,
+  EnrichmentTaskStatusView,
 )
 
 app_name = 'csv_manager'
@@ -11,4 +13,6 @@ urlpatterns = [
     path('csv-files/', CSVFileListCreateView.as_view(), name='csv_file_list_create'),
     path('csv-files/<int:pk>/headers/', CSVFileRetrieveHeaderView.as_view(), name='csv_file_headers'),
     path('csv-files/<int:pk>/content/', CSVFileRetrieveContentView.as_view(), name='csv_file_content'),
+    path('csv-files/<int:pk>/enrich/', CSVFileEnrichView.as_view(), name='csv_file_enrich'),
+    path('enrichment-tasks/<str:task_id>/', EnrichmentTaskStatusView.as_view(), name='enrichment_task_status'),
 ]
